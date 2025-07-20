009e475ea2e184f25ea752650fa07fc357a3bdc3
Mohan B <81377388+mohanwithdata@users.noreply.github.com>
2025-07-20 14:18:50 +0000

    initi

--- customer_churn_api/train_model.py
+++ customer_churn_api/train_model.py
@@ -5,6 +5,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import joblib
 
-
 df = pd.read_csv('customer_churn_api/data/WA_Fn-UseC_-Telco-Customer-Churn.csv')
-df.head(3)
+
+# Preprocessing
+df.dropna(inplace=True)
+df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+df.fillna(0, inplace=True)
+
+# Encode categorical variables
+categorical_cols = df.select_dtypes(include=['object']).columns
+le = LableEncoder()
+
+for col in categorical_cols:
+    df[col] = le.fit_transform(df[col])
+
+
+
